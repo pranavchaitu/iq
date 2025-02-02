6bbccf205bf4750644623017306d5a8b771e5f14
pranavchaitu <pranavchaitu676@gmail.com>
2025-02-02 15:34:27 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+const port = process.env.PORT
+
+const app = express()
+
+app.get('/',(req,res) => {
+    res.json({
+        'msg' : "hello"
+    })
+})
+
+
+
+app.listen(port,() => {
+    console.log('listening to port:',port);
+})
+
+
+
