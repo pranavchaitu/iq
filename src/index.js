19ab8dab71fc8384b448cccbdd6e4a77aebf99a8
krishnakanth <krishnakanthpathi@gmail.com>
2025-03-07 08:13:25 +0530

    strated models

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ import problem_route from './routes/problem.route.js';
 import doubt_route from './routes/doubt.route.js';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json()); // middleware to parse json data
 
 // user routes
@@ -23,8 +25,8 @@ app.use("/api/problems" , problem_route)
 app.use("/api/doubts" , doubt_route)
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
 
